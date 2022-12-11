5a9e40a60adc6f07911f4a8a593e242c20c71d2b
Pierre-Chesne <pierrc@hotmail.com>
2022-12-11 17:57:36 +0100

    create posgreSQL

---
+++ DB/Infra/PosgreSQL.sh
@@ -0,0 +1,50 @@
+RESOURCE_GROUP="RG_Rugby_Stats_V2"
+LOCATION="westeurope"
+POSTGRESQL_NAME="rugbystatsv2"
+POSTGRESQL_ADMINUSER="adminDB"
+POSTGRESQL_ADMINPASSWORD="Password123$"
+POSTGRESQL_SKUNAME="Standard_B1ms"
+POSTGRESQL_TIER="Burstable"
+POSTGRESQL_VERSION="14"
+POSTGRESQL_STORAGESIZE="32"
+POSTGRESQL_DBNAME="rugby_api"
+
+# Create a resource group.
+az group create \
+  --name $RESOURCE_GROUP \
+  --location $LOCATION
+
+# Create a logical server in the resource group.
+az postgres flexible-server create \
+  --name $POSTGRESQL_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --location $LOCATION \
+  --admin-user $POSTGRESQL_ADMINUSER \
+  --admin-password $POSTGRESQL_ADMINPASSWORD \
+  --sku-name $POSTGRESQL_SKUNAME \
+  --tier $POSTGRESQL_TIER \
+  --version $POSTGRESQL_VERSION \
+  --storage-size $POSTGRESQL_STORAGESIZE \
+  --public-access 0.0.0.0 --yes
+
+# Firewall rule to allow access from internet
+az postgres flexible-server firewall-rule create \
+  --name $POSTGRESQL_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --rule-name allowall \
+  --start-ip-address 0.0.0.0 \
+  --end-ip-address 255.255.255.255
+
+# Create Database
+az postgres flexible-server db create \
+  --resource-group $RESOURCE_GROUP \
+  --server-name $POSTGRESQL_NAME \
+  --database-name $POSTGRESQL_DBNAME
+
+# create table sum_plaquage_ok int
+az postgres flexible-server execute \
+  --admin-password $POSTGRESQL_ADMINPASSWORD \
+  --admin-user $POSTGRESQL_ADMINUSER \
+  --name $POSTGRESQL_NAME \
+  --database-name $POSTGRESQL_DBNAME \
+  --file-path create_tables.sql
